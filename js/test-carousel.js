efa1b3a0d5a2120bd41d111a302665df0ec268f6
Synytska <pankiv38@gmail.com>
2022-09-29 17:53:44 +0100

    added new carousel

--- js/test-carousel.js
+++ js/test-carousel.js
@@ -1,65 +1,81 @@
+// const images = document.querySelectorAll('#testimonials__roller .test-carousel-photos .testimonials__person');
+// const line = document.querySelector('.test-carousel-photos');
+// let count = 0;
+// let width;
+
+// function init() {
+//     console.log('resize');
+//     width = document.querySelector('#testimonials__roller').offsetWidth;
+//     line.style.width = width * images.length + "px";
+//     images.forEach(item => {
+//         item.style.width = width + "px";
+//         item.style.height = 'auto';
+//     });
+//     roll();
+// }
+
+// window.addEventListener('resize', init);
+// init();
+
+// document.querySelector('.testim_next').addEventListener('click', function () {
+//     count++;
+//     if (count >= images.length) {
+//         count = 0;
+//     }
+//     roll();
+// });
+
+// document.querySelector('.testim_prev').addEventListener('click', function () {
+//     count--;
+//     if (count < 0) {
+//         count = images.length - 1;
+//     }
+//     roll();
+// });
+
+// function roll() {
+//     line.style.transform = 'translate(-' + count * width + 'px)';
+// }
 
 
-/*let btn_prev = document.querySelector('#testimonials__roller .carousel-buton .prev');
-let images = document.querySelectorAll('#testimonials__roller .test-carousel-photos .testimonials__person');
-let btn_next = document.querySelector('#testimonials__roller .carousel-buton .next');
+(function(){
+    const slides = document.querySelectorAll('#testimonials__roller .test-carousel-photos .testimonials__person');
+let curentSlide = 0;
 
-let i = 0;
-images[i].style.display = 'none';
-i++;
-if (i >= images.length) {
-    i = 0;
-}
-btn_prev.onclick = function () {
-    images[i].style.display = 'none';
-    i = i - 1;
-    if (i < 0) {
-        i = images.length - 1;
+const slideContainer = document.querySelector('#testimonials__roller .test-carousel-photos .testimonials__person');
+
+function renderSlide() {
+    slideContainer.innerHTML = slides[curentSlide];
+    if(window.innerWidth > 600){
+        const secondSlide = curentSlide + 1 >= slides.length ? 0 :curentSlide + 1;
+        slideContainer.innerHTML += slides[secondSlide];
+        if(window.innerWidth > 900) {
+        const thirdSlide =  secondSlide + 1 >= slides.length ? 0 :secondSlide + 1;
+        slideContainer.innerHTML += slides[thirdSlide];
+        
+        }
     }
-    images[i].style.display = 'block';
-} */
-
-const images = document.querySelectorAll('#testimonials__roller .test-carousel-photos .testimonials__person');
-const line = document.querySelector('.test-carousel-photos');
-let count = 0;
-let width;
-
-function init() {
-    console.log('resize');
-    width = document.querySelector('#testimonials__roller').offsetWidth;
-    line.style.width = width * images.length + "px";
-    images.forEach(item => {
-        item.style.width = width + "px";
-        item.style.height = 'auto';
-    });
-    roll();
+}
+function nextSlide() {
+    curentSlide = curentSlide + 1 >= slides.length ? 0 : curentSlide + 1;
+renderSlide();
+}
+function prevSlide() {
+    curentSlide = curentSlide - 1 < 0 ? slides.length -1 : curentSlide -1;
+renderSlide();
 }
 
-window.addEventListener('resize', init);
-init();
+renderSlide();
+setInterval(nextSlide, 3000);
 
-document.querySelector('.testim_next').addEventListener('click', function () {
-    count++;
-    if (count >= images.length) {
-        count = 0;
-    }
-    roll();
-});
+const btnNext = document.querySelector('.testim_next');
+btnNext.addEventListener('click', nextSlide);
 
-document.querySelector('.testim_prev').addEventListener('click', function () {
-    count--;
-    if (count < 0) {
-        count = images.length - 1;
-    }
-    roll();
-});
+const btnPrev = document.querySelector('.testim_prev');
+btnPrev.addEventListener('click', prevSlide);
 
-function roll() {
-    line.style.transform = 'translate(-' + count * width + 'px)';
-}
-
-
-
+window.addEventListener('resize', renderSlide);
+})();
 
 
 
