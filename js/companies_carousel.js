ad5d5430ecef1b507659ae1260d19cae257820b2
Yaroslav Yenkala <yaroslavrick@gmail.com>
2022-10-01 21:24:32 +0300

    slider done on the Carousel Companies

--- js/companies_carousel.js
+++ js/companies_carousel.js
@@ -27,10 +27,95 @@
     }
 
     if (window.innerWidth >= 768) {
-      const thirdSlideIdx = slideIdx + 2 >= slides.length ? 1 : slideIdx + 2;
-      const fourthSlideIdx = slideIdx + 3 >= slides.length ? 2 : slideIdx + 3;
-      slideContainer.innerHTML +=
-        slides[thirdSlideIdx] + slides[fourthSlideIdx];
+      // const thirdSlideIdx = slideIdx + 2 >= slides.length ? 0 : slideIdx + 2;
+
+      let thirdSlideIdx;
+      if (slideIdx === 7) {
+        thirdSlideIdx = 0;
+      } else if (slideIdx === 8) {
+        thirdSlideIdx = 1;
+      } else {
+        thirdSlideIdx = slideIdx + 2;
+      }
+
+      slideContainer.innerHTML += slides[thirdSlideIdx];
+
+      // const fourthSlideIdx = slideIdx + 3 >= slides.length ? 0 : slideIdx + 3;
+
+      let fourthSlideIdx;
+      if (slideIdx === 6) {
+        fourthSlideIdx = 0;
+      } else if (slideIdx === 7) {
+        fourthSlideIdx = 1;
+      } else if (slideIdx === 8) {
+        fourthSlideIdx = 2;
+      } else {
+        fourthSlideIdx = slideIdx + 3;
+      }
+
+      slideContainer.innerHTML += slides[fourthSlideIdx];
+    }
+
+    if (window.innerWidth >= 992) {
+      // const fifthSlideIdx =
+      //   slideIdx + 4 >= slideContainer.length ? 3 : slideIdx + 4;
+
+      let fifthSlideIdx;
+      if (slideIdx === 5) {
+        fifthSlideIdx = 0;
+      } else if (slideIdx === 6) {
+        fifthSlideIdx = 1;
+      } else if (slideIdx === 7) {
+        fifthSlideIdx = 2;
+      } else if (slideIdx === 8) {
+        fifthSlideIdx = 3;
+      } else {
+        fifthSlideIdx = slideIdx + 4;
+      }
+
+      slideContainer.innerHTML += slides[fifthSlideIdx];
+
+      // const sixthSlideIdx =
+      //   slideIdx + 5 >= slideContainer.length ? 3 : slideIdx + 5;
+
+      let sixthSlideIdx;
+      if (slideIdx === 4) {
+        sixthSlideIdx = 0;
+      } else if (slideIdx === 5) {
+        sixthSlideIdx = 1;
+      } else if (slideIdx === 6) {
+        sixthSlideIdx = 2;
+      } else if (slideIdx === 7) {
+        sixthSlideIdx = 3;
+      } else if (slideIdx === 8) {
+        sixthSlideIdx = 4;
+      } else {
+        sixthSlideIdx = slideIdx + 5;
+      }
+
+      slideContainer.innerHTML += slides[sixthSlideIdx];
+
+      // const seventhSlideIdx =
+      //   slideIdx + 6 >= slideContainer.length ? 3 : slideIdx + 6;
+
+      let seventhSlideIdx;
+      if (slideIdx === 3) {
+        seventhSlideIdx = 0;
+      } else if (slideIdx === 4) {
+        seventhSlideIdx = 1;
+      } else if (slideIdx === 5) {
+        seventhSlideIdx = 2;
+      } else if (slideIdx === 6) {
+        seventhSlideIdx = 3;
+      } else if (slideIdx === 7) {
+        seventhSlideIdx = 4;
+      } else if (slideIdx === 8) {
+        seventhSlideIdx = 5;
+      } else {
+        seventhSlideIdx = slideIdx + 6;
+      }
+
+      slideContainer.innerHTML += slides[seventhSlideIdx];
     }
   }
 
