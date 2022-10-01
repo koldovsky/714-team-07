7d61a8397d72125d9329a2319eb665f58f684b3b
Yaroslav Yenkala <yaroslavrick@gmail.com>
2022-10-01 16:04:36 +0300

    working on the Carousel Companies

--- js/companies_carousel.js
+++ js/companies_carousel.js
@@ -1,14 +1,14 @@
 (function () {
   const slides = [
-    `<div><img src="img/subscribe_section/small/slider_airtel.png" alt="Airtel" /></div>`,
-    `<div><img src="img/subscribe_section/small/slider_argo.png" alt="Argo" /></div>`,
-    `<div><img src="img/subscribe_section/small/slider_bobo_coworking.png" alt="Bobo" /></div>`,
-    `<div><img src="img/subscribe_section/small/slider_bone.png" alt="Bone" /></div>`,
-    `<div><img src="img/subscribe_section/small/slider_didi_insider.png" alt="DiDi" /></div>`,
-    `<div><img src="img/subscribe_section/small/slider_ideas_first.png" alt="Ideas First" /></div>`,
-    `<div><img src="img/subscribe_section/small/slider_just_in_time.png" alt="Just In Time" /></div>`,
-    `<div><img src="img/subscribe_section/small/slider_paragon.png" alt="Paragon" /></div>`,
-    `<div><img src="img/subscribe_section/small/slider_southern_company.png" alt="Southern Company" /></div>`,
+    `<div class="slide-container__logo_div"><img class="logo_div__img" src="img/subscribe_section/small/slider_airtel.png" alt="Airtel" /></div>`,
+    `<div class="slide-container__logo_div"><img class="logo_div__img" src="img/subscribe_section/small/slider_argo.png" alt="Argo" /></div>`,
+    `<div class="slide-container__logo_div"><img class="logo_div__img" src="img/subscribe_section/small/slider_bobo_coworking.png" alt="Bobo" /></div>`,
+    `<div class="slide-container__logo_div"><img class="logo_div__img" src="img/subscribe_section/small/slider_bone.png" alt="Bone" /></div>`,
+    `<div class="slide-container__logo_div"><img class="logo_div__img" src="img/subscribe_section/small/slider_didi_insider.png" alt="DiDi" /></div>`,
+    `<div class="slide-container__logo_div"><img class="logo_div__img" src="img/subscribe_section/small/slider_ideas_first.png" alt="Ideas First" /></div>`,
+    `<div class="slide-container__logo_div"><img class="logo_div__img" src="img/subscribe_section/small/slider_just_in_time.png" alt="Just In Time" /></div>`,
+    `<div class="slide-container__logo_div"><img class="logo_div__img" src="img/subscribe_section/small/slider_paragon.png" alt="Paragon" /></div>`,
+    `<div class="slide-container__logo_div"><img class="logo_div__img" src="img/subscribe_section/small/slider_southern_company.png" alt="Southern Company" /></div>`,
   ];
 
   let slideIdx = 0;
@@ -21,9 +21,16 @@
     slideContainer.innerHTML = slides[slideIdx];
 
     // Responsive:
-    if (window.innerWidth >= 700) {
+    if (window.innerWidth >= 480) {
       const secondSlideIdx = slideIdx + 1 >= slides.length ? 0 : slideIdx + 1;
       slideContainer.innerHTML += slides[secondSlideIdx];
+    }
+
+    if (window.innerWidth >= 768) {
+      const thirdSlideIdx = slideIdx + 2 >= slides.length ? 1 : slideIdx + 2;
+      const fourthSlideIdx = slideIdx + 3 >= slides.length ? 2 : slideIdx + 3;
+      slideContainer.innerHTML +=
+        slides[thirdSlideIdx] + slides[fourthSlideIdx];
     }
   }
 
@@ -37,7 +44,7 @@
     showCurrentSlide();
   }
 
-  setInterval(nextSlide, 4000);
+  // setInterval(nextSlide, 4000);
   showCurrentSlide();
 
   const nextButton = document.querySelector(
