83823f70ba05e29d30e559d625cfdc4521450878
Maksym <maximv_89@hotmail.com>
2022-10-03 21:08:08 +0300

    optimized organi Counter

--- js/organicCounter.js
+++ js/organicCounter.js
@@ -1,11 +1,11 @@
 (function () {
-    const divs = document.querySelectorAll('.achievement__count');
-
-    for (const div of divs) {
-        setInterval(() => {
-            let count = div.innerHTML;
-            count++;
-            div.innerHTML = count;
-        }, 5000);   
+    const divs = document.querySelectorAll(".achievement__count");
+    function counter() {
+      for (const div of divs) {
+        let count = div.innerHTML;
+        count++;
+        div.innerHTML = count;
+      }
     }
-})();
+    setInterval(counter, 5000);
+  })();
